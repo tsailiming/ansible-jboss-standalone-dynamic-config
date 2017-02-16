c720dd116c696f1ee55bb89abccc0aa143055e2c
Tsai Li Ming <tsai.liming@gmail.com>
2017-02-16 11:21:57 +0800

    Sample Tower API script

---
+++ bin/tower-api.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from time import sleep
+
+TOWER_IP='10.42.0.42'
+USERNAME='admin'
+PASSWORD='kXtKsGuyjajh'
+JOB_TEMPLATE_ID='46'
+
+
+NEXUS_REPO_URL='http://54.169.3.13:8081/repository/demo'
+STAGE='sit'
+
+URL='http://' + TOWER_IP + '/api/v1/job_templates/' + JOB_TEMPLATE_ID + '/launch/'
+payload = dict(nexus_repo_url=NEXUS_REPO_URL, stage=STAGE)
+headers = {'Content-type': 'application/json'}
+r = requests.post(URL, 
+                    data=json.dumps(payload),
+                    headers=headers,
+                    auth=HTTPBasicAuth(USERNAME, PASSWORD))
+job_url = 'http://' + TOWER_IP + r.json()['url']
+
+print job_url
+
+while True:
+
+    r = requests.get(job_url,
+                        auth=HTTPBasicAuth(USERNAME, PASSWORD)).json()
+    
+    status = r['status']
+    print 'Job Status: ' + status
+
+    if status in ['successful', 'failed']:
+        break
+    else:
+        sleep(5)
+
+
